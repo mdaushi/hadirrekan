63a8870e7e1902426b4360aaa388dfb8507849ec
muhfirdaus19 <firdaus.ex19@gmail.com>
2022-03-21 10:27:37 +0800

    improve: redirect to login page

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,7 @@ use App\Http\Controllers\PresensiController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/login');
 
 Route::middleware('auth')->group(function(){
     Route::get('dashboard', function(){
