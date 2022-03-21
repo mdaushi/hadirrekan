4c24b6085203d811e0aa333ac16571497b2d5edc
muhfirdaus19 <firdaus.ex19@gmail.com>
2022-03-21 10:34:49 +0800

    change:credit footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,11 +1,11 @@
 <footer>
     <div class="footer clearfix mb-0 text-muted">
         <div class="float-start">
-            <p>2021 &copy; Mazer</p>
+            <p><?php echo Date('Y'); ?> &copy; HADIRREKAN</p>
         </div>
         <div class="float-end">
             <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
-                by <a href="https://ahmadsaugi.com">Saugi</a></p>
+                by PW IPNU SULSEL</p>
         </div>
     </div>
 </footer>
